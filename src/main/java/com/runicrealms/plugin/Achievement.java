d362e75065347bae53895f68d44603148d94adc6
Skyfallin <skyfallin.business@gmail.com>
2022-10-28 21:19:49 -0400

    basic enum setup

--- src/main/java/com/runicrealms/plugin/Achievement.java
+++ src/main/java/com/runicrealms/plugin/Achievement.java
@@ -1,4 +1,46 @@
 package com.runicrealms.plugin;
 
 public enum Achievement {
+
+    FISH_10_COD();
+
+    private final String id;
+    private final String name;
+    private final String description;
+    private final int pointValue;
+    private final AchievementType achievementType;
+    private final RewardType rewardType;
+
+    Achievement(String id, String name, String description, int pointValue, AchievementType achievementType, RewardType rewardType) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.pointValue = pointValue;
+        this.achievementType = achievementType;
+        this.rewardType = rewardType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public AchievementType getAchievementType() {
+        return achievementType;
+    }
+
+    public RewardType getRewardType() {
+        return rewardType;
+    }
 }
