69545b30690b333b576c5e0f191b07b9393c84ab
Skyfallin <skyfallin.business@gmail.com>
2022-10-31 08:36:45 -0400

    a few performance improvements

--- src/main/java/com/runicrealms/plugin/gui/AchievementGUIListener.java
+++ src/main/java/com/runicrealms/plugin/gui/AchievementGUIListener.java
@@ -9,7 +9,6 @@ import com.runicrealms.plugin.model.AchievementData;
 import com.runicrealms.plugin.model.TitleData;
 import com.runicrealms.plugin.reward.TitleReward;
 import com.runicrealms.plugin.utilities.GUIUtil;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -18,7 +17,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
-import redis.clients.jedis.Jedis;
 
 public class AchievementGUIListener implements Listener {
 
@@ -55,21 +53,14 @@ public class AchievementGUIListener implements Listener {
             if (achievementStatus == null) return;
             if (!achievementStatus.isUnlocked()) return;
             player.closeInventory();
-            try (Jedis jedis = RunicCore.getRedisManager().getJedisResource()) {
-                for (Reward reward : achievement.getRewards()) {
-                    if (!(reward instanceof TitleReward)) continue;
-                    TitleReward titleReward = (TitleReward) reward;
-                    TitleData titleData = RunicCore.getTitleManager().loadTitleData(player.getUniqueId());
-                    if (titleReward.isSuffix()) {
-                        titleData.setSuffix(titleReward.getTitle());
-                        titleData.writeTitleDataToJedis(jedis);
-                    } else {
-                        Bukkit.broadcastMessage("writing title");
-                        titleData.setPrefix(titleReward.getTitle()); // todo: bundle these 3 (add expiry) in a method
-                        // todo: don't think this is getting properly memoized on login
-                        titleData.writeTitleDataToJedis(jedis);
-                        Bukkit.broadcastMessage(titleData.getPrefix() + " is prefix");
-                    }
+            for (Reward reward : achievement.getRewards()) {
+                if (!(reward instanceof TitleReward)) continue;
+                TitleReward titleReward = (TitleReward) reward;
+                TitleData titleData = RunicCore.getTitleManager().loadTitleData(player.getUniqueId());
+                if (titleReward.isSuffix()) {
+                    titleData.setSuffix(titleReward.getTitle());
+                } else {
+                    titleData.setPrefix(titleReward.getTitle());
                 }
             }
         }
