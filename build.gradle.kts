7eaeb2ef4082a72f667ed772ebcd5d176862574b
Excel619 <excel8392@gmail.com>
2023-05-18 16:22:33 -0600

    Added build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "com.runicrealms.plugin"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    compileOnly(commonLibs.paper)
+    compileOnly(commonLibs.spigot)
+    compileOnly(commonLibs.jedis)
+    compileOnly(commonLibs.mythicmobs)
+    compileOnly(commonLibs.craftbukkit)
+    compileOnly(commonLibs.worldguardevents)
+    compileOnly(project(":Projects:Core"))
+    compileOnly(project(":Projects:Guilds"))
+    compileOnly(project(":Projects:Items"))
+    compileOnly(project(":Projects:Professions"))
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.runicrealms.plugin"
+            artifactId = "achievements"
+            version = "1.0-SNAPSHOT"
+            from(components["java"])
+        }
+    }
+}
+
+tasks.register("wrapper")
+tasks.register("prepareKotlinBuildScriptModel")
